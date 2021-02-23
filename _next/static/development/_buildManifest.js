e0a14cf90a78c4fe9178876996ef2f01a795e064
Kojiro Ozaki <koujirou0111@gmail.com>
2021-02-24 04:54:26 +0900

    Updates

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/feeds/[feedId]":["static\u002Fchunks\u002Fpages\u002Ffeeds\u002F[feedId].js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Ffeeds\u002F[feedId]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
